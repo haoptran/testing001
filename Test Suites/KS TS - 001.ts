c5a49fda6e5bc76a2deaad1a7c3d0d0cb6c0f9c1
Hao Tran <hao.ptran@katalon.com>
2023-10-04 13:31:51 +0700

    KS TS - 001 002

---
+++ Test Suites/KS TS - 001.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>KS TS - 001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>363cc7d8-0695-4e97-9245-b194358f5bd2</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/KS TS - 002.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>KS TS - 002</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>92a997bf-ee72-4335-a8b8-126ec89edaa0</testSuiteGuid>
+</TestSuiteEntity>
